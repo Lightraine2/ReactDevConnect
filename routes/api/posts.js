eb8551977b3281689566c60e1e985e4e171fced1
Lightraine2 <laurencedale1986@gmail.com>
2019-06-05 18:49:54 +0100

    User routes added

---
+++ routes/api/posts.js
@@ -0,0 +1,12 @@
+// Handle user profiles
+
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/posts
+// @desc    Test route
+// @access  Public
+
+router.get('/', (req, res) => res.send('Posts route'));
+
+module.exports = router;
