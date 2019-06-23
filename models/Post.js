714b9cc2126cc495283f90e65f3ce787815cd4e0
Lightraine2 <laurencedale1986@gmail.com>
2019-06-23 22:07:16 +0100

    Add posts

--- models/Post.js
+++ models/Post.js
@@ -35,7 +35,7 @@ const PostSchema = new Schema({
                 required: true
             },
             avatar: {
-                type: string
+                type: String
             },
             date: {
                 type: Date,
